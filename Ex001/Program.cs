55f56a2d892201a40df939c30af588eb5561b964
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-01-30 20:30:51 +0300

    add Start

---
+++ Ex001/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+if(int.TryParse(ReadLine(), out int num));
+{
+    Console.WriteLine("Введите число");
+}
