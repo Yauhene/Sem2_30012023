b6d2e8017f09a2408e9ec360d29b601820194e2f
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-01-30 20:49:12 +0300

    add Рабочая версия

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -1,6 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите число");
-if(int.TryParse(ReadLine(), out int num));
-{
-    Console.WriteLine("Введите число");
-}
+int num = new Random().Next(100,999);
+//if(int.TryParse(ReadLine(), out int num));
+//{
+    //Console.WriteLine("Введите число");
+//}
+Console.WriteLine(num);
+int first = num % 10;
+Console.WriteLine(first);
+int third = num / 100;
+Console.WriteLine(third);
+int res=first+third*10;
+Console.WriteLine(res);
+
