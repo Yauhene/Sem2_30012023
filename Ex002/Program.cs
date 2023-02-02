ef44de473818212a8cb40edec58302e9f20a3472
Yauheni Aviltsau <Y.Aviltsau@Weisrussland.loc>
2023-02-02 16:03:38 +0300

    add Ex002 gone

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,24 @@
 ﻿// Прога принимает 2 числа и проверяет, кратно ли первое второму, если нет - выводит остаток
+
+
+Console.WriteLine();
+Console.WriteLine();
+
+
 Console.WriteLine("Введите первое число");
+int Dig1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int Dig2 = int.Parse(Console.ReadLine());
+
+if (Dig1%Dig2==0)
+{
+    Console.WriteLine("Число "+ Dig1 + " кратно числу " + Dig2);
+}
+else
+{
+    Console.WriteLine("Число "+ Dig1 + " не кратно числу " + Dig2);
+}
+
+Console.WriteLine();
+Console.WriteLine();
